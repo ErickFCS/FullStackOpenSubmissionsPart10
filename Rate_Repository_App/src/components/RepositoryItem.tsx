38c93b2abb802ecf6bbc20dbcdc0ed54a9617c59
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-06 17:05:18 -0300

    Part 10 the reviewed repositories list

---
+++ Rate_Repository_App/src/components/RepositoryItem.tsx
@@ -0,0 +1,40 @@
+import { View, Text } from "react-native";
+
+interface PropType {
+    id: string;
+    fullName: string;
+    description: string;
+    language: string;
+    forksCount: number;
+    stargazersCount: number;
+    ratingAverage: number;
+    reviewCount: number;
+    ownerAvatarUrl: string;
+}
+
+const RepositoryItem = (prop: PropType) => {
+    const {
+        id,
+        fullName,
+        description,
+        language,
+        forksCount,
+        stargazersCount,
+        ratingAverage,
+        reviewCount,
+        ownerAvatarUrl,
+    } = prop;
+    return (
+        <View>
+            <Text>Full name: {fullName}</Text>
+            <Text>Description: {description}</Text>
+            <Text>Language: {language}</Text>
+            <Text>Stars: {stargazersCount}</Text>
+            <Text>Forks: {forksCount}</Text>
+            <Text>Reviews: {reviewCount}</Text>
+            <Text>Rating: {ratingAverage}</Text>
+        </View>
+    )
+};
+
+export default RepositoryItem;
