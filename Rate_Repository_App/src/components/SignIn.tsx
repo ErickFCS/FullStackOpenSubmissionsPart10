b6c012545d52f7ff19053d161f618cf67f09c843
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-07 12:07:43 -0300

    Part 10 validating the sign-in form

--- Rate_Repository_App/src/components/SignIn.tsx
+++ Rate_Repository_App/src/components/SignIn.tsx
@@ -2,6 +2,7 @@ import { Pressable, StyleSheet, TextInput, View } from 'react-native';
 import { Text } from './Text';
 import theme from '../theme';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const styles = StyleSheet.create({
     container: {
@@ -16,6 +17,19 @@ const styles = StyleSheet.create({
         padding: 10,
         margin: 10,
         color: theme.colors.bsInfoTextEmphasis,
+    },
+    inputError: {
+        backgroundColor: theme.colors.bsDangerBgSubtle,
+        borderColor: theme.colors.bsDangerBorderSubtle,
+        borderWidth: 1,
+        padding: 10,
+        margin: 10,
+        color: theme.colors.bsDangerTextEmphasis,
+    },
+    inputErrorText: {
+        color: theme.colors.bsDangerTextEmphasis,
+        marginStart: 20,
+        marginBottom: 10,
     },
     button: {
         backgroundColor: theme.colors.bsSuccessBgSubtle,
@@ -36,6 +50,10 @@ const SignIn = () => {
             username: '',
             password: '',
         },
+        validationSchema: Yup.object().shape({
+            username: Yup.string().required('UserName is Required'),
+            password: Yup.string().required('PassWord is Required')
+        }),
         onSubmit: () => {
             console.log('Form submitted');
             console.log('UserName', formkit.values.username);
@@ -46,19 +64,25 @@ const SignIn = () => {
         <View style={styles.container}>
             <TextInput
                 placeholder='UserName'
-                placeholderTextColor={theme.colors.bsInfoTextEmphasis}
-                style={styles.input}
+                placeholderTextColor={formkit.errors.username && formkit.touched.username ? theme.colors.bsDangerTextEmphasis : theme.colors.bsInfoTextEmphasis}
+                style={formkit.errors.username && formkit.touched.username ? styles.inputError : styles.input}
                 value={formkit.values.username}
                 onChangeText={formkit.handleChange('username')}
             />
+            {formkit.errors.username && formkit.touched.username ?
+                <Text style={styles.inputErrorText}>{formkit.errors.username}</Text>
+                : null}
             <TextInput
                 placeholder='PassWord'
-                placeholderTextColor={theme.colors.bsInfoTextEmphasis}
+                placeholderTextColor={formkit.errors.password && formkit.touched.password ? theme.colors.bsDangerTextEmphasis : theme.colors.bsInfoTextEmphasis}
                 secureTextEntry
-                style={styles.input}
+                style={formkit.errors.password && formkit.touched.password ? styles.inputError : styles.input}
                 value={formkit.values.password}
                 onChangeText={formkit.handleChange('password')}
             />
+            {formkit.errors.password && formkit.touched.password ?
+                <Text style={styles.inputErrorText}>{formkit.errors.password}</Text>
+                : null}
             <Pressable style={styles.button} onPress={(e) => { formkit.handleSubmit() }}>
                 <Text style={styles.buttonText}>Sign In</Text>
             </Pressable>
