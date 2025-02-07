39a9401b7fee6f4d326e88fd499fed1441297b51
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-07 11:12:32 -0300

    Part 10 the sign-in form

--- Rate_Repository_App/src/components/SignIn.tsx
+++ Rate_Repository_App/src/components/SignIn.tsx
@@ -1,7 +1,69 @@
+import { Pressable, StyleSheet, TextInput, View } from 'react-native';
 import { Text } from './Text';
+import theme from '../theme';
+import { useFormik } from 'formik';
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+    },
+    input: {
+        backgroundColor: theme.colors.bsDarkBgSubtle,
+        borderColor: theme.colors.bsBorderColor,
+        borderWidth: 1,
+        padding: 10,
+        margin: 10,
+        color: theme.colors.bsInfoTextEmphasis,
+    },
+    button: {
+        backgroundColor: theme.colors.bsSuccessBgSubtle,
+        borderColor: theme.colors.bsSuccessBorderSubtle,
+        borderWidth: 1,
+        padding: 12,
+        margin: 10,
+    },
+    buttonText: {
+        color: theme.colors.bsSuccessTextEmphasis,
+        textAlign: 'center'
+    },
+})
 
 const SignIn = () => {
-    return <Text>The sign-in view</Text>;
+    const formkit = useFormik({
+        initialValues: {
+            username: '',
+            password: '',
+        },
+        onSubmit: () => {
+            console.log('Form submitted');
+            console.log('UserName', formkit.values.username);
+            console.log('PassWord', formkit.values.password);
+        }
+    })
+    return (
+        <View style={styles.container}>
+            <TextInput
+                placeholder='UserName'
+                placeholderTextColor={theme.colors.bsInfoTextEmphasis}
+                style={styles.input}
+                value={formkit.values.username}
+                onChangeText={formkit.handleChange('username')}
+            />
+            <TextInput
+                placeholder='PassWord'
+                placeholderTextColor={theme.colors.bsInfoTextEmphasis}
+                secureTextEntry
+                style={styles.input}
+                value={formkit.values.password}
+                onChangeText={formkit.handleChange('password')}
+            />
+            <Pressable style={styles.button} onPress={(e) => { formkit.handleSubmit() }}>
+                <Text style={styles.buttonText}>Sign In</Text>
+            </Pressable>
+        </View>
+    )
 };
 
 export default SignIn;
