31f9ecf4970433d79950b707dd099566843c76ed
Erick Fernando Carvalho Sanchez <carvalhosanchezerickfernando@gmail.com>
2025-02-11 12:52:55 -0300

    Part 10 fetching repositories with Apollo Client

--- Rate_Repository_App/src/hooks/useRepositories.ts
+++ Rate_Repository_App/src/hooks/useRepositories.ts
@@ -1,4 +1,6 @@
+import { useQuery } from '@apollo/client';
 import { useState, useEffect } from 'react';
+import { GET_REPOSITORIES } from '../graphql/querys';
 
 interface Repo {
     id: string;
@@ -29,27 +31,35 @@ interface Res {
     }
 }
 
+const USE_FETCH = false;
+
 const useRepositories = () => {
     const [repositories, setRepositories] = useState<Repo[]>([]);
-    const [loading, setLoading] = useState(false);
 
-    const fetchRepositories = async () => {
-        setLoading(true);
-
-        const res = await fetch('http://192.168.1.42:5000/api/repositories');
-        const rawData: Res = await res.json();
-        const data = rawData.edges.map((e) => (e.node));
-        setRepositories(data);
-
-        setLoading(false);
-        setRepositories(data);
-    };
-
-    useEffect(() => {
-        fetchRepositories();
-    }, []);
-
-    return { repositories, loading, refetch: fetchRepositories };
+    if (USE_FETCH) {
+        const [loading, setLoading] = useState(false);
+        const fetchRepositories = async () => {
+            setLoading(true);
+            const res = await fetch('http://192.168.1.42:5000/api/repositories');
+            const rawData: Res = await res.json();
+            const data = rawData.edges.map((e) => (e.node));
+            setRepositories(data);
+            setLoading(false);
+            setRepositories(data);
+        };
+        useEffect(() => {
+            fetchRepositories();
+        }, []);
+        return { repositories, loading, refetch: fetchRepositories };
+    } else {
+        const repos = useQuery(GET_REPOSITORIES, {
+            fetchPolicy: 'cache-and-network',
+            onCompleted(data) {
+                setRepositories(data.repositories?.edges?.map((e) => (e.node)));
+            },
+        });
+        return { ...repos, repositories };
+    }
 };
 
 export default useRepositories;
